7c72d8fd3a899fa65aa852b35d320801e94c63ec
alfonpunk <alfonsoferrertorres@gmail.com>
2026-08-08 12:36:40 +0200

    fix: repair syntax error in api/v1/leads.js fallback handler

--- api/v1/leads.js
+++ api/v1/leads.js
@@ -1,2 +1,5 @@
 import licenseHandler from '../license.js';
-export default async function handler(req, res) { return licenseHandler(req, res); }
+
+export default async function handler(req, res) {
+  return licenseHandler(req, res);
+}
